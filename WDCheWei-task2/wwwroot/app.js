92b54730316646aebb64d9ee0ea1615036fba37f
KingOfSpicy <sangowosan@gmail.com>
2022-03-02 10:53:14 +0200

    Add working files, this work was web design starter.

---
+++ WDCheWei-task2/wwwroot/app.js
@@ -0,0 +1,42 @@
+﻿function colorChange(x) {
+    x.style.backgroundColor = "red";
+    }
+function colorBack(x) {
+    x.style.backgroundColor = "#0404B4";
+}
+function writeTXT(y) {
+    y.style.color = "#6A0888";
+    y.style.fontWeight = "bold";
+}
+function writeBack(y) {
+    y.style.color = "black";
+    y.style.fontWeight = "normal";
+}
+/*function colorSwitch() {
+    document.getElementById("demo").style.color = "red";
+}
+function colorReturn() {
+    document.getElementById("demo").style.color = "green";*/
+/*function sizeChange(x) {
+    x.style.height = "480px";
+    x.style.width = "750px";
+}
+function sizeBack(x) {
+    x.style.height = "320px";
+    x.style.width = "500px";*/
+/*$(document).ready(function colorChange(x) {
+    $('x').mouseenter(function () {
+        $('x').fadeTo('fast', 1);
+    });
+});*/
+/*function bigImg(Luffy) {
+    Luffy.style.height = "480px";
+    Luffy.style.width = "750px";
+}*/
+/*function colorChange(div) {
+    div.style.backgroundColor = "red";
+}
+img.onmouseOver = function () {
+    img.style.height = "480px";
+    img.style.width = "750px";
+}*/
